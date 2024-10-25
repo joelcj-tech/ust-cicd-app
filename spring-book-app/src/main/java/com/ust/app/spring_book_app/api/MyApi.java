ea55bca109bd5944f50506090f88ea516e8f0ddd
Joel.CJames <joel.cjames@ust.com>
2024-10-25 10:31:06 +0530

    Spring book app first commit

---
+++ spring-book-app/src/main/java/com/ust/app/spring_book_app/api/MyApi.java
@@ -0,0 +1,13 @@
+package com.ust.app.spring_book_app.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyApi {
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello World !";
+    }
+}
