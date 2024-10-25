2c28c2b5c9f0b198f30791952cb016396a4008ff
Joel.CJames <joel.cjames@ust.com>
2024-10-25 12:58:58 +0530

    fresh update

---
+++ src/test/java/com/ust/app/spring_book_app/SpringBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.app.spring_book_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
